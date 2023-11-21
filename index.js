50ca2d957e0c0346ee0abba54eadd0da5b7c4b48
andy <andy@gmail.com>
2023-11-21 20:18:06 +0800

    change marker color

--- index.js
+++ index.js
@@ -280,7 +280,17 @@ function setPoint() {
     const localArray = local.split(",");
     const localLatlng = L.latLng(localArray[0], localArray[1]);
     console.log(localLatlng);
-    marker2 = L.marker(localLatlng, { draggable: false });
+    marker2 = L.marker(localLatlng, {
+        draggable: false, icon: L.icon(
+            {
+                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+                shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+            })
+    });
     marker2.addTo(map);
 }
 
