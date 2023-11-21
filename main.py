a8581cff8a078104cd642dbfaab0727c64c03bab
Andy <w84a832k6@gmail.com>
2023-11-21 13:26:01 +0800

    change port

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def server(tunnel_host, tunnel_port):
         clients[sid][0].service.close()
         clients.pop(sid)
 
-    s = eventlet.listen(('localhost', 3000))
+    s = eventlet.listen(('localhost', 3001))
     [ip, port] = s.getsockname()
     print('--port', port)
     eventlet.wsgi.server(s, app)
